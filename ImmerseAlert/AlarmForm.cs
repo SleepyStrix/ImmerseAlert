6671f9e0a9cd3798be98be18da16a88d1c876718
Dan Hewlett <danchewlett@sleepystrix.com>
2017-02-18 13:48:09 -0600

    Initial Commit

---
+++ ImmerseAlert/AlarmForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImmerseAlert
+{
+    public partial class AlarmForm : Form
+    {
+        Thread keepFrontThread;
+        public AlarmForm()
+        {
+            InitializeComponent();
+            Console.WriteLine("ALARM FORM STARTED");
+            PlayAlarmSound();
+            keepFrontThread = new Thread(KeepFront);
+            keepFrontThread.Start();
+
+        }
+
+        private void PlayAlarmSound()
+        {
+            SoundPlayer alarmPlayer = new SoundPlayer(@"C:\Users\Dan\Source\Repos\ImmerseAlert\ImmerseAlert\Audio\Alarm1.wav");
+            alarmPlayer.PlayLooping();
+        }
+
+        private void KeepFront()
+        {
+            while (true)
+            {
+                try
+                {
+                    MethodInvoker mi = delegate ()
+                    {
+                        ForceToFront();
+                    };
+                    this.Invoke(mi);
+                } catch (Exception e)
+                {
+                    Console.WriteLine("Keep Front Exception: " + e.GetBaseException().Message);
+                    break;
+                }                
+                Thread.Sleep(5000);
+            }
+        }
+
+        public void ForceToFront()
+        {
+            Console.WriteLine("Forcing Alarm to front");
+            try
+            {
+                this.WindowState = FormWindowState.Minimized;
+                this.Show();
+                this.WindowState = FormWindowState.Maximized;
+                this.Focus();
+                this.BringToFront();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Force To Front Exception: " + e.GetBaseException().Message);
+            }
+        }
+    }
+}
---
+++ ImmerseAlert/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImmerseAlert
+{
+    static class Program
+    {
+        private static bool alarmActive = false;
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Thread.Sleep(20000);
+            ShowAlarm();
+
+        }
+
+        private static void ShowAlarm()
+        {
+            alarmActive = true;
+            AlarmForm alarmForm = new AlarmForm();
+            Application.Run(alarmForm);
+        }
+    }
+}
